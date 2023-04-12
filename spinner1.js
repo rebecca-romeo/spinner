022bccd28ae24423499df5cc0586192303aae11b
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-12 07:04:17 -0400

    implemented spinner function

---
+++ spinner1.js
@@ -0,0 +1,28 @@
+// Demonstrates the \r rewrite character:
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+// ... fill in the rest yourself ...
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1100);
